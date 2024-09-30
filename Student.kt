64cec5f303c4bcf682e337a8f2d41b0049951415
Viliebe <147209751+Viliebe@users.noreply.github.com>
2024-09-30 06:19:56 +0300

    ЛР1, Зад 5. Представление аргументов конструктора в форме ХЕШа.

--- Student.kt
+++ Student.kt
@@ -1,80 +1,128 @@
 class Student {
-    companion object {
-        private var lastId: Int = 0
-    }
-
-    var id: Int = 0
-        private set
-
-    var surname: String = ""
-        set(value) {
-            field = value
+    var id: Int =-1
+        set(value)
+        {
+            if(value>0)
+                field=value
         }
-        get() {
+        get()
+        {
+            return field
+        }
+    var surname: String =""
+        set(value)
+        {
+            field=value
+        }
+        get()
+        {
+            return field
+        }
+    var name: String =""
+        set(value)
+        {
+            field=value
+        }
+        get()
+        {
+            return field
+        }
+    var patronymic: String =""
+        set(value)
+        {
+            field=value
+        }
+        get()
+        {
+            return field
+        }
+    var phone: String? =null
+        set(value)
+        {
+            field=value
+        }
+        get()
+        {
             return field
         }
 
-    var name: String = ""
-        set(value) {
-            field = value
+    var telegram: String? =null
+        set(value)
+        {
+            field=value
         }
-        get() {
+        get()
+        {
             return field
         }
 
-    var patronymic: String = ""
-        set(value) {
-            field = value
+    var mail: String? =null
+        set(value)
+        {
+            field=value
         }
-        get() {
+        get()
+        {
             return field
         }
 
-    var phone: String? = null
-        set(value) {
-            field = value
+    var git: String? =null
+        set(value)
+        {
+            field=value
         }
-        get() {
+        get()
+        {
             return field
         }
 
-    var mail: String? = null
-        set(value) {
-            field = value
-        }
-        get() {
-            return field
-        }
-
-    var git: String? = null
-        set(value) {
-            field = value
-        }
-        get() {
-            return field
-        }
-
-    constructor(_surname: String, _name: String, _patronymic: String) {
-        surname = _surname
-        name = _name
-        patronymic = _patronymic
-        lastId++
-        id = lastId
-    }
-    constructor(_id:Int=-1,_surname:String,_name:String,_patronymic:String,_phone:String?=null,_mail:String?=null,_git:String?="")
+    companion object
     {
-        id=_id
+        var ids=0
+    }
+
+    init
+    {
+        ids++
+    }
+
+    constructor(_surname:String,_name:String,_patronymic:String)
+    {
+        id=ids
+        surname=_surname
+        name=_name
+        patronymic=_patronymic
+    }
+    constructor(_surname:String,_name:String,_patronymic:String,_phone:String?=null,_telegram:String?=null,_mail:String?=null,_git:String?=null)
+    {
+        id=ids
         surname=_surname
         name=_name
         patronymic=_patronymic
         phone=_phone
+        telegram=_telegram
         mail=_mail
         git=_git
     }
+
+    constructor(hashStud: HashMap<String,Any?>)
+    {
+        id=ids
+        surname=hashStud["surname"].toString()
+        name=hashStud["name"].toString()
+        patronymic=hashStud["patronymic"].toString()
+        phone=hashStud.getOrDefault("phone",null).toString()
+        telegram=hashStud.getOrDefault("telegram",null).toString()
+        mail=hashStud.getOrDefault("mail",null).toString()
+        git=hashStud.getOrDefault("git",null).toString()
+
+    }
+
     fun write()
     {
-        var out = "ID: $id, Фамиля: $surname, Имя: $name, Отчество: $patronymic"
+        var out = "ID: $id, Фамилия: $surname, Имя: $name, Отчество: $patronymic"
         if(phone!=null)out+=", Телефон: $phone"
+        if(telegram!=null)out+=", Телеграм: $phone"
         if(mail!=null)out+=", Почта: $mail"
         if(git!=null)out+=", Гит: $git"
         println(out)
