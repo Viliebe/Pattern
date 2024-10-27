d32294cd5bdb407d57b764cb9eb7303919e7cef2
Viliebe <147209751+Viliebe@users.noreply.github.com>
2024-10-28 01:10:33 +0300

    ЛР2, Зад 12. Добавлены методы в класс DataTable.

--- DataTable.kt
+++ DataTable.kt
@@ -1,2 +1,15 @@
 class DataTable(private val data: Array<Array<Any?>>) {
+    fun getElement(row:Int,column:Int):Any?
+    {
+        return data.getOrNull(row)?.getOrNull(column)
+    }
+    fun getRows():Int
+    {
+        return data.size
+    }
+
+    fun getColumns():Int
+    {
+        return data[0].size
+    }
 }
--- Main.kt
+++ Main.kt
@@ -9,24 +9,29 @@ class Main {
         println(ravil.validate())
         ravil.setContacts("89999999999","wrongtelegram","mail@yandex.ru")
         println(ravil.toString())
-        var ivanInp= hashMapOf<String,Any?>(
-            "lastname" to "Ivanko",
-            "name" to "Ivan",
-            "fathername" to "Ivanevich"
-        )
-        var ivan=Student(ivanInp)
-        println(ivan.toString())
-        var jenek=Student("Evgeniy Prokopenko Evgenievich")
-        println(jenek.toString())
-        println(ravil.getInfo())
-        var rav=StudentShort(ravil)
-        println(rav.toString())
-        var studList= Student.readFromTxt("input.txt")
-        for (stud in studList)
-        {
-            println(stud)
-        }
-        Student.writeToTxt("output.txt",studList)
+//                var ivanInp= hashMapOf<String,Any?>(
+//            "surname" to "Ivanov
+//            "name" to "Ivan",
+//            "patronymic" to "Ivanovich"
+//        )
+//        var ivan=Student(ivanInp)
+//        println(ivan.toString())
+//
+//        var danila=Student("Daniil Danilanin Danilavich")
+//        println(danila.toString())
+//        println(ravil.getInfo())
+//        var vik=StudentShort(ravil)
+//        println(rav.toString())
+//        var studList= Student.readFromTxt("input.txt")
+//        for (stud in studList)
+//        {
+//            println(stud)
+//        }
+//        Student.writeToTxt("output.txt",studList)
+        var dtt=DataTable(arrayOf(arrayOf(1,2,3), arrayOf(4,5,6),arrayOf("a","b",3)))
+        println(dtt.getElement(2,2))
+        println(dtt.getRows())
+        println(dtt.getColumns())
     }
 }
 fun main() = Main().main()
